f7ea83a3e0854701e6c64557d8cd24c46b1e0d79
sagarjadhavwebdev <jadhavsagar.live@gmail.com>
2022-04-03 00:09:26 +0530

    s

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,12 @@ import Header from "../componets/Header";
 import Footer from "../componets/Footer";
 import useFetch from "use-http";
 
-  if (typeof window !== "undefined") {
-    const module = await import("flowbite")
-    return module;
-  }
+if (typeof window !== "undefined") {
+  import("flowbite");
+}
 
 export default function Home() {
-  const {data} = useFetch("/api/hello", []);
+  const { data } = useFetch("/api/hello", []);
   const products = data?.products;
   return (
     <>
