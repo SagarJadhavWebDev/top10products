9ab2a39ca1e2fea5be99580d1a409053e40cc3f4
sagrjadhavwebdev <jadhavsagar.live@gmail.com>
2022-04-03 20:49:03 +0530

    s

--- src/componets/Footer.tsx
+++ src/componets/Footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
               alt="Flowbite Logo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              ProductBite
             </span>
           </a>
           <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 mb-0 dark:text-gray-400">
